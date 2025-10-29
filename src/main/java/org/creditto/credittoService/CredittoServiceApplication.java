3f4da948710c57fe496072ae8e93529ea9480856
Jeyong <dlwpdyd201@naver.com>
2025-10-29 23:27:43 +0900

    init: 프로젝트 초기설정

---
+++ src/main/java/org/creditto/credittoService/CredittoServiceApplication.java
@@ -0,0 +1,13 @@
+package org.creditto.credittoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredittoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredittoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/creditto/credittoService/CredittoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.creditto.credittoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredittoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
